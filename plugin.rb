531bcdedb9fd708cebce522cdec530d5459c228a
U-Momon-PC\Momon <muhlisbc@gmail.com>
2018-05-26 00:07:27 +0700

    Prototype

---
+++ plugin.rb
@@ -0,0 +1,10 @@
+# name: hide-suggested-topics
+# version: 0.1
+# author: Muhlis Budi Cahyono (muhlisbc@gmail.com)
+# url: https://github.com/muhlisbc
+
+enabled_site_setting :hide_suggested_topics_enabled
+
+after_initialize {
+
+}
